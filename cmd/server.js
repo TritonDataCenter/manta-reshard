dd24a5e0d50112ba387f03e01212762bbcadbbba
Joshua M. Clulow <jmc@joyent.com>
2017-11-01 00:01:34 +0000

    basic startup sequence and remote DC discovery

--- cmd/server.js
+++ cmd/server.js
@@ -1,21 +1,392 @@
 
-var mod_bunyan = require('bunyan');
+var mod_fs = require('fs');
+var mod_path = require('path');
 
-var LOG;
+var mod_assert = require('assert-plus');
+var mod_bunyan = require('bunyan');
+var mod_vasync = require('vasync');
+var mod_verror = require('verror');
+var mod_jsprim = require('jsprim');
+var mod_ufds = require('ufds');
+var mod_sdc = require('sdc-clients');
+
+
+var VE = mod_verror.VError;
+
+function
+retry(func, ctx, done, nsecs)
+{
+	setTimeout(function () {
+		func(ctx, done);
+	}, nsecs * 1000);
+}
+
+function
+get_sapi_application(log, sapi, name, include_master, callback)
+{
+	mod_assert.object(log, 'log');
+	mod_assert.object(sapi, 'sapi');
+	mod_assert.string(name, 'name');
+	mod_assert.bool(include_master, 'include_master');
+	mod_assert.func(callback, 'callback');
+
+	var opts = {
+		name: name
+	};
+
+	if (include_master) {
+		/*
+		 * Note that SAPI merely checks for the _presence_ of
+		 * "include_master" in the query string.  Passing any value,
+		 * even "false", results in the inclusion of master results.
+		 */
+		opts.include_master = true;
+	}
+
+	sapi.listApplications(opts, function (err, apps) {
+		if (err) {
+			callback(new VE(err, 'locate "%s" application', name));
+			return;
+		}
+
+		if (apps.length !== 1) {
+			callback(new VE('found %d "%s" applications, wanted 1',
+			    apps.length, name));
+			return;
+		}
+
+		callback(null, apps[0]);
+	});
+}
+
+
+function
+load_config(ctx, done)
+{
+	ctx.ctx_log.debug('loading configuration file "%s"', ctx.ctx_cfgfile);
+	mod_fs.readFile(ctx.ctx_cfgfile, function (err, data) {
+		if (err) {
+			if (err.code === 'ENOENT') {
+				if (ctx.ctx_cfgfile_notfound++ === 0) {
+					ctx.ctx_log.info('waiting for ' +
+					    'config file "%s"',
+					    ctx.ctx_cfgfile);
+				}
+
+				/*
+				 * Wait for the file to exist.
+				 */
+				retry(load_config, ctx, done, 1);
+				return;
+			}
+
+			done(new VE(err, 'loading file "%s"', ctx.ctx_cfgfile));
+			return;
+		}
+
+		var out;
+		try {
+			out = JSON.parse(data.toString('utf8'));
+		} catch (ex) {
+			done(new VE(ex, 'parsing file "%s"', ctx.ctx_cfgfile));
+			return;
+		}
+
+		ctx.ctx_log.info({ config: out },
+		    'loaded configuration from "%s"', ctx.ctx_cfgfile);
+
+		ctx.ctx_cfg = out;
+
+		setImmediate(done);
+	});
+}
+
+function
+create_ufds_client(ctx, done)
+{
+	var ufdsopts = mod_jsprim.deepCopy(ctx.ctx_cfg.ufds);
+
+	ufdsopts.log = ctx.ctx_log.child({ component: 'ufds' });
+
+	ctx.ctx_ufds = new mod_ufds(ufdsopts);
+
+	var finished = false;
+	var finish = function (err) {
+		if (finished) {
+			return;
+		}
+		finished = true;
+
+		if (err) {
+			done(new VE(err, 'could not connect to UFDS'));
+		} else {
+			done();
+		}
+	};
+
+	ctx.ctx_ufds.once('connect', finish);
+	ctx.ctx_ufds.once('connectError', finish);
+}
+
+function
+create_sapi_client(ctx, done)
+{
+	ctx.ctx_sapi = new mod_sdc.SAPI({
+		url: ctx.ctx_cfg.sapi_url,
+		log: ctx.ctx_log.child({ component: 'sapi' }),
+		agent: false
+	});
+
+	setImmediate(done);
+}
+
+function
+find_datacentres(ctx, done)
+{
+	ctx.ctx_ufds.listDatacenters(ctx.ctx_cfg.region, function (err, dcs) {
+		if (err) {
+			ctx.ctx_log.warn(err, 'could not list ' +
+			    'datacentres (retrying)');
+			retry(find_datacentres, ctx, done, 5);
+			return;
+		}
+
+		ctx.ctx_log.debug({ dcs: dcs }, 'datacentre list');
+
+		var out = {};
+
+		dcs.forEach(function (dc) {
+			mod_assert.strictEqual(dc.region, ctx.ctx_cfg.region,
+			    'unexpected region');
+			mod_assert.string(dc.datacenter, 'dc.datacenter');
+			mod_assert.ok(!out[dc.datacenter]);
+
+			out[dc.datacenter] = {
+				dc_region: dc.region,
+				dc_name: dc.datacenter,
+				dc_sapi: null,
+				dc_cnapi: null,
+				dc_app: null,
+				dc_clients: {
+					dcc_sapi: null,
+					dcc_urclient: null,
+				}
+			};
+		});
+
+		ctx.ctx_log.info({ dcs: out }, 'found datacentres');
+		ctx.ctx_dcs = out;
+
+		done();
+	}, true);
+}
+
+/*
+ * Use our local SAPI to get a list of all instances of all Manta services.
+ */
+function
+load_manta_application(ctx, done)
+{
+	get_sapi_application(ctx.ctx_log, ctx.ctx_sapi, 'manta', true,
+	    function (err, app) {
+		if (err) {
+			ctx.ctx_log.warn(err, 'could not locate ' +
+			    '"manta" SAPI application (retrying)');
+			retry(load_manta_application, ctx, done, 5);
+			return;
+		}
+
+		ctx.ctx_app = app;
+
+		ctx.ctx_log.info('found manta application "%s"', app.uuid);
+		ctx.ctx_log.trace({ manta_app: app }, 'manta application');
+
+		mod_assert.strictEqual(ctx.ctx_app.metadata.REGION,
+		    ctx.ctx_cfg.region, 'REGION');
+
+		done();
+	});
+}
+
+function
+load_manta_objects(ctx, done)
+{
+	ctx.ctx_sapi.getApplicationObjects(ctx.ctx_app.uuid,
+	    { include_master: true }, function (err, res) {
+		if (err) {
+			ctx.ctx_log.warn(err, 'could not load ' +
+			    'SAPI objects for "manta" application (retrying)');
+			retry(load_manta_objects, ctx, done, 5);
+			return;
+		}
+
+		mod_assert.object(res.services, 'services');
+		mod_assert.object(res.instances, 'instances');
+
+		ctx.ctx_log.trace({ services: res.services }, 'services!');
+
+		ctx.ctx_services = res.services;
+		ctx.ctx_instances = res.instances;
+
+		done();
+	});
+}
+
+function
+find_sapi_urls(ctx, done)
+{
+	var err = null;
+
+	mod_assert.object(ctx.ctx_instances, 'ctx_instances');
+
+	mod_jsprim.forEachKey(ctx.ctx_instances, function (us, insts) {
+		mod_jsprim.forEachKey(insts, function (ui, inst) {
+			var dc = ctx.ctx_dcs[inst.metadata.DATACENTER];
+
+			if (!dc) {
+				err = new VE('instance "%s" is in unknown ' +
+				    'DC "%s"', ui, inst.metadata.DATACENTER);
+				return;
+			}
+
+			if (dc.dc_sapi !== null && dc.dc_sapi !==
+			    inst.metadata.SAPI_URL) {
+				err = new VE('instance "%s" has different ' +
+				    'SAPI URL "%s"',
+				    ui, inst.metadata.SAPI_URL);
+				return;
+			}
+
+			dc.dc_sapi = inst.metadata.SAPI_URL;
+		});
+	});
+
+	mod_jsprim.forEachKey(ctx.ctx_dcs, function (n, dc) {
+		if (dc.dc_sapi === null) {
+			err = new VE('could not find SAPI URL for DC "%s"', n);
+			return;
+		}
+
+		ctx.ctx_log.info({ dc_sapi_url: dc.dc_sapi },
+		     'found SAPI URL for DC "%s"', n);
+	});
+
+	if (err) {
+		setImmediate(done, err);
+		return;
+	}
+
+	ctx.ctx_log.info('found SAPI URLs for all DCs');
+	setImmediate(done);
+}
+
+function
+create_remote_sapi_clients(ctx, done)
+{
+	mod_jsprim.forEachKey(ctx.ctx_dcs, function (n, dc) {
+		dc.dc_clients.dcc_sapi = new mod_sdc.SAPI({
+			url: dc.dc_sapi,
+			log: ctx.ctx_log.child({ component: 'sapi/' +
+			    dc.dc_name }),
+			agent: false
+		});
+	});
+
+	setImmediate(done);
+}
+
+function
+load_remote_sdc_applications(ctx, done)
+{
+	mod_vasync.forEachPipeline({ inputs: Object.keys(ctx.ctx_dcs),
+	    func: function (n, next) {
+		var dc = ctx.ctx_dcs[n];
+
+		get_sapi_application(ctx.ctx_log, dc.dc_clients.dcc_sapi,
+		    'sdc', false, function (err, app) {
+			if (err) {
+				next(new VE(err, 'remote DC "%s"', n));
+				return;
+			}
+
+			dc.dc_app = app;
+
+			ctx.ctx_log.info('found "sdc" app %s in DC "%s"',
+			    dc.dc_app.uuid, n);
+
+			next();
+		});
+
+	} }, function (err) {
+		if (err) {
+			ctx.ctx_log.warn(err, 'could not load remote ' +
+			    '"sdc" SAPI applications (retrying)');
+			retry(load_remote_sdc_applications, ctx, done, 5);
+			return;
+		}
+
+		setImmediate(done);
+	});
+}
+
 
 (function
 main()
 {
-	LOG = mod_bunyan.createLogger({
+	var ctx = {
+		ctx_cfgfile: mod_path.join(__dirname, '..', 'etc',
+		    'config.json'),
+		ctx_cfgfile_notfound: 0
+	};
+
+	var log = ctx.ctx_log = mod_bunyan.createLogger({
 		name: 'reshard',
 		level: process.env.LOG_LEVEL || mod_bunyan.DEBUG,
 		serializers: mod_bunyan.stdSerializers
 	});
 
-	LOG.info('service start');
+	log.info('starting up');
+	mod_vasync.pipeline({ arg: ctx, funcs: [
+		/*
+		 * Load the configuration file from disk as soon as it
+		 * exists.
+		 */
+		load_config,
 
-	/*
-	 * For now, hold the process open.
-	 */
-	setInterval(function () {}, 120 * 1000);
+		/*
+		 * To complete our initial configuration, we need a UFDS and
+		 * SAPI client for the local datacentre.
+		 */
+		create_ufds_client,
+		create_sapi_client,
+
+		/*
+		 * Get the list of datacentres in this region from UFDS.  We
+		 * use this to determine if this is a single- or multi-DC
+		 * deployment, as well as to find the full list of DCs for
+		 * which we need SAPI clients.
+		 */
+		find_datacentres,
+
+		/*
+		 * Load the full set of SAPI instances for Manta.  We coalesce
+		 * the metadata for each instance to collect the full set of
+		 * SAPI URLs for all DCs.
+		 */
+		load_manta_application,
+		load_manta_objects,
+
+		find_sapi_urls,
+		create_remote_sapi_clients,
+
+		load_remote_sdc_applications,
+
+	] }, function (err) {
+		if (err) {
+			log.fatal(err, 'startup failure');
+			process.exit(1);
+		}
+
+		log.info('startup complete');
+	});
 })();
