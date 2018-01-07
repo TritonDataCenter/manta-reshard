f7a6074fb1776022b86444f13af10baf3b995704
Joshua M. Clulow <jmc@joyent.com>
2018-01-07 00:42:25 +0000

    prototype "status -r" for live redraw of status

--- cmd/status.js
+++ cmd/status.js
@@ -98,32 +98,64 @@ fetch_status(ip, port, callback)
 	req.end();
 }
 
-fetch_status('127.0.0.1', 80, function (err, res) {
+function
+print_status(redraw, callback)
+{
+	fetch_status('127.0.0.1', 80, function (err, res) {
+		if (redraw) {
+			console.log('\u001b[H\u001b[2JDATE: %s\n',
+			    (new Date()).toISOString());
+		}
+
+		if (err) {
+			callback(err);
+			return;
+		}
+
+		if (res.problems.length > 0) {
+			console.log('PROBLEMS:');
+			res.problems.forEach(function (problem) {
+				console.log('  » %s', problem);
+			});
+			console.log('');
+		}
+
+		if (!res.plans) {
+			callback(new VE('RESULT DID NOT INCLUDE "plans"'));
+			return;
+		}
+
+		var k = Object.keys(res.plans).sort();
+
+		for (var i = 0; i < k.length; i++) {
+			var p = res.plans[k[i]];
+
+			lib_status.pretty_print(p.status);
+
+			console.log('');
+		}
+
+		callback();
+	});
+}
+
+function
+redraw_callback(err)
+{
+	if (err) {
+		console.log('ERROR: %s', VE.fullStack(err));
+	}
+	setTimeout(print_status, 500, true, redraw_callback);
+}
+
+var redraw = false;
+if (process.argv[2] === '-r') {
+	redraw = true;
+}
+
+print_status(redraw, redraw ? redraw_callback : function (err) {
 	if (err) {
 		console.error('ERROR: %s', VE.fullStack(err));
 		process.exit(1);
-	}
-
-	if (res.problems.length > 0) {
-		console.log('PROBLEMS:');
-		res.problems.forEach(function (problem) {
-			console.log('  » %s', problem);
-		});
-		console.log('');
-	}
-
-	if (!res.plans) {
-		console.error('RESULT DID NOT INCLUDE "plans"');
-		process.exit(1);
-	}
-
-	var k = Object.keys(res.plans).sort();
-
-	for (var i = 0; i < k.length; i++) {
-		var p = res.plans[k[i]];
-
-		lib_status.pretty_print(p.status);
-
-		console.log('');
 	}
 });
